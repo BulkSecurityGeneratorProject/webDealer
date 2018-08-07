c3924cb57283c6c763a9255f145dd1227b55a934
karol_drozdowski@o2.pl <karol_drozdowski@o2.pl>
2018-08-07 15:38:10 +0200

    Merge branch 'dev' of C:\ddweb\webDealer2\webDealer with conflicts.

--- src/main/webapp/app/shared/login/login.component.ts
+++ src/main/webapp/app/shared/login/login.component.ts
@@ -55,7 +55,7 @@ export class JhiLoginModalComponent implements AfterViewInit {
             this.authenticationError = false;
             if (this.router.url === '/register' || (/^\/activate\//.test(this.router.url)) ||
                 (/^\/reset\//.test(this.router.url))) {
-                this.router.navigate(['']);
+                this.router.navigate(['home']);
             }
 
             this.eventManager.broadcast({
