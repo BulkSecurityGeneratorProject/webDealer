f6e8fabf0320d23680fcc9f6f260d4ca61854027
karol_drozdowski@o2.pl <karol_drozdowski@o2.pl>
2018-08-17 14:17:11 +0200

    Dodane image z dysku.
    Image nalezy odpalic przez tomcata i umiescic folder /image w webapps a w nim /cars a w nim No img.png

--- src/main/webapp/app/entities/home/home.component.ts
+++ src/main/webapp/app/entities/home/home.component.ts
@@ -44,6 +44,37 @@ export class HomeComponent implements OnInit {
         this.loadAll();
         this.registerAuthenticationSuccess();
         this.registerChangeInHome();
+        // this.fixInternetExplorerParallax();
+    }
+    fixInternetExplorerParallax() {
+        if (navigator.userAgent.match(/Trident\/7\./)) {
+            $('body').on('mousewheel', function () {
+                event.preventDefault();
+
+                const wheelDelta = event['wheelDelta'];
+
+                const currentScrollPosition = window.pageYOffset;
+                window.scrollTo(0, currentScrollPosition - wheelDelta);
+            });
+
+            $('body').keydown(function (e) {
+                e.preventDefault(); // prevent the default action (scroll / move caret)
+                const currentScrollPosition = window.pageYOffset;
+
+                switch (e.which) {
+
+                    case 38: // up
+                        window.scrollTo(0, currentScrollPosition - 120);
+                        break;
+
+                    case 40: // down
+                        window.scrollTo(0, currentScrollPosition + 120);
+                        break;
+
+                    default: return; // exit this handler for other keys
+                }
+            });
+        }
     }
     loadAll() {
         this.homeService.get().subscribe(
